3e099212926c739792dc04cf8bbccd5c53054017
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-14 23:22:44 +0530

    Added Modules example

---
+++ Modules/hello-bye.py
@@ -0,0 +1,15 @@
+import messages
+# import messages as msg
+# msg.bye()
+# msg.hello()
+###########################
+
+#from messages import *
+#from messages import hello,bye
+#hello()
+#bye()
+
+help("modules")
+
+messages.hello()
+messages.bye()
---
+++ Modules/messages.py
@@ -0,0 +1,9 @@
+#a module is a file containing Python definitions and statements. Modules allow you to organize your code into reusable files, making your programs more modular and manageable. You can use built-in modules that come with Python or create your own custom modules
+
+def hello():
+    print("Hello! Have a nice day")
+
+def bye():
+    print("Have a Great time!")
+
+#Now create a another file called name.py and import messages module to call these functions
