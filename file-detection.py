f346a6e25baf00f6090d676f9b25d703235d1b04
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-14 17:07:18 +0530

    file-detection.py

--- file-detection.py
+++ file-detection.py
@@ -1,12 +1,15 @@
 import os
 
+# Define the path to check
 path = "C:\\Users\\Admin\\Desktop\\Jenkins-Shared-Library"
 
+# Check if the path exists
 if os.path.exists(path):
-    print("Location exists")
+    print("Location exists")  # Path exists
+    # Check if the path is a file or directory
     if os.path.isfile(path):
-        print("That is a file")
+        print("That is a file")  # It's a file
     elif os.path.isdir(path):
-        print("That is a directory")
+        print("That is a directory")  # It's a directory
 else:
-    print("File does not exist")
+    print("File does not exist")  # Path doesn't exist
